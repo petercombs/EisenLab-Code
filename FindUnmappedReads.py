cf70002ba171d109bd3ecd3f270bb3ce1cc0cb7e
Peter Combs <peter.combs@berkeley.edu>
2011-11-01 16:50:49 -0700

    Add timing and fix bugs
    Trim the /1 or /2 off of the reads so that they actually match the
    IDs in the sam tools.
    
    Count down from 2 if the id is found (not 4), so that both the quality
    and sequence don't add 4.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Your branch is ahead of 'origin/master' by 1 commit.
    #
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   FindUnmappedReads.py
    #

--- FindUnmappedReads.py
+++ FindUnmappedReads.py
@@ -1,16 +1,19 @@
 from __future__ import print_function
 import sys
+import time
 
-
+start_lib = time.time()
 ids = {line.split()[0] for i, line in enumerate(file(sys.argv[1]))
       if ((i % 1000000 == 0 and print('.', file=sys.stderr, end=' ')) or True)}
+
+print(time.time() - start_lib, file=sys.stderr)
 
 printlines = 0
 
 for line in file(sys.argv[2]):
-    id = line.split()[0][1:]
+    id = line.split()[0][1:-2]
     if id not in ids:
-        printlines = 4
+        printlines = 2
 
     if printlines:
         print(line.strip())
