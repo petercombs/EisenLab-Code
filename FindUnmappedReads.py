1e48a8a917f7df197aaf3025364b036bbcda9c23
Peter Combs <peter.combs@berkeley.edu>
2011-11-03 14:36:10 -0700

    Change dictionary to index on sequence and fix algorithm
    Indexing on sequence should allow us to use the filtered_hits.sam file
    instead of accepted_hits, which is (unfortunately), an order of
    magnitude smaller.
    
    Also, the way I look for lines to keep should actually work now, as
    opposed to before, when it would see a non-ID line and deicide that it
    didn't match.
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch PAC03
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   FindUnmappedReads.py
    #

--- FindUnmappedReads.py
+++ FindUnmappedReads.py
@@ -3,18 +3,23 @@ import sys
 import time
 
 start_lib = time.time()
-ids = {line.split()[0] for i, line in enumerate(file(sys.argv[1]))
+# Sequence is in the 9th column, ID in the 0th
+ids = {line.split()[9] for i, line in enumerate(file(sys.argv[1]))
       if ((i % 1000000 == 0 and print('.', file=sys.stderr, end=' ')) or True)}
 
 print(time.time() - start_lib, file=sys.stderr)
 
 printlines = 0
 
-for line in file(sys.argv[2]):
-    if printlines == 0:
-        id = line.split()[0][1:-2]
-        if id not in ids:
-            printlines = 4
-    else:
-        print(line.strip())
-        printlines -= 1
+grab_next = False
+for i, line in enumerate(file(sys.argv[2])):
+    # Sequence line
+    if i % 4 == 1 and line.strip() not in ids:
+        seq = line.strip()
+        grab_next = True
+    if grab_next:
+        id = line.strip().split()[0].replace('+', '>')
+        print id
+        print seq
+        grab_next = False
+
