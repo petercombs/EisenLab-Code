e1211736823ab8d9328b3ebd1ff2adcdfb22de4e
Peter Combs <peter.combs@berkeley.edu>
2012-01-06 12:46:48 -0800

    Add hist_sorted to PlotUtils
    When plotting multiple datasets, this should put the shortest one in
    front.  Untested thus far, it's possible I have it backwards!
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch PAC06
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   PlotUtils.py
    #
    # Changes not staged for commit:
    #   (use "git add <file>..." to update what will be committed)
    #   (use "git checkout -- <file>..." to discard changes in working directory)
    #
    #	modified:   EstimateComplexity.py
    #

--- PlotUtils.py
+++ PlotUtils.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as mpl
 from matplotlib import cm
 from collections import defaultdict
 from scipy.stats import spearmanr, pearsonr, gaussian_kde
-from numpy import log, array, Inf, median, exp
+from numpy import log, array, Inf, median, exp, argsort
 
 
 def loglog_heat(x,y, **kwargs):
@@ -28,6 +28,31 @@ def loglog_heat(x,y, **kwargs):
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
     return retval
+
+
+def hist_sorted(*args, labels=None, **kwargs):
+    all_ns = []
+    all_patches = []
+
+    if not labels:
+        labels = ['data %d' % (i+1) for i in range(len(args))]
+    elif len(labels) != len(args):
+        raise ValueError('length of labels not equal to length of data')
+
+    for data, label in zip(args, labels):
+        ns, bins, patches = mpl.hist(data, label=label, **kwargs)
+        all_ns.append(ns)
+        all_patches.append(patches)
+    z_orders = argsort(all_ns, axis=0)
+
+    for zrow, patchrow in zip(z_orders, all_patches):
+        assert len(zrow) == len(patchrow)
+        for z_val, patch in zip(zrow, patchrow):
+            patch.set_zorder(z_val)
+
+    return all_ns, bins, all_patches
+
+
 
 
 
