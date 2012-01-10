39bdcb97f02973c9e60b2601f2df3e0c4a9b99b8
Peter Combs <peter.combs@berkeley.edu>
2012-01-10 14:38:39 -0800

    Regularize data in FindDifferenceCounts
    Data as is may not be the same shape, and therefore can't be made into
    an array.  Fortunately, the structure of the code in TSSFinder is such
    that if a column is missing, it's always going to be at the end, so I
    can just pad with zeros.
    
    Except I can't just pad with zeros, so I introduce pseudo-counts.  Who
    knows if that's actually the correct thing to do, and will introduce
    errors in the analysis downstream in the case of zero-expression in the
    middle.  Meh.
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch PAC06
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   FindDifferenceCounts.py
    #
    # Untracked files:
    #   (use "git add <file>..." to include in what will be committed)
    #
    #	TSSFinder.py

--- FindDifferenceCounts.py
+++ FindDifferenceCounts.py
@@ -1,16 +1,30 @@
 import sys
 from cPickle import load
 from scipy import stats
+from numpy import array, shape
 
 if __name__ == "__main__":
     slices = []
     for fname in sys.argv[1:]:
-        genes.append(load(open(fname)))
+        slices.append(load(open(fname)))
 
     for gene in slices[0]:
         data = [slice[gene] for slice in slices]
-        chi2, p, dof, expected = stats.chi2_contingency(data)
-        if p < (.05 / len(slices[0])):
-            print gene, data
+        n = max(len(i) for i in data)
+        for i in data:
+            while len(i) < n:
+                i.append(0)
+
+        data = array(data, dtype=float)
+        rows, cols = shape(data)
+        # Pseudocounts for fixing low-count data
+        data += 2.0 / cols
+
+        try:
+            chi2, p, dof, expected = stats.chi2_contingency(data)
+            if p < (.05 / (len(slices[0])* len(sys.argv[1:]))):
+                print gene, data
+        except ValueError:
+            print "Failed on: ", gene, data
 
 
