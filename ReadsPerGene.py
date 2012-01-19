4a80ca3d855584c23fff78913e569e88e0356bda
Peter Combs <peter.combs@berkeley.edu>
2012-01-19 11:34:13 -0800

    Try to actually do this by FBgn, not FBtr
    Before, I was just assuming the FBgn ID was the last one in the line,
    which is not true (It's an FBtr).  This was likely leading to all kinds
    of whacky duplicate issues.

--- ReadsPerGene.py
+++ ReadsPerGene.py
@@ -4,18 +4,29 @@ from os import path
 import cPickle as pickle
 import subprocess
 import os
+import re
 
 chrs = defaultdict(lambda : defaultdict(list))
+fbgn_finder = re.compile('FBgn[0-9]+')
+fbtr_finder = re.compile('FBtr[0-9]+')
+
+fbtr_to_fbgn = {}
 
 for line in open(argv[1]):
     try:
         data = line.split('\t')
         chr = data[0]
         kind = data[2]
+        if fbgn_finder.findall(line) and fbtr_finder.findall(line):
+            fbtr_to_fbgn[fbtr_finder.findall(line)[0]] = \
+                fbgn_finder.findall(line)[0]
         if kind != 'exon': continue
         start = int(data[3])
         stop = int(data[4])
-        fbgn = data[-1][-12:-2]
+        try:
+            fbgn = fbtr_to_fbgn[fbtr_finder.findall(line)[0]]
+        except KeyError:
+            fbgn = 'ERR'
         if chr not in chrs:
             print chr
         for i in range(start, stop+1):
