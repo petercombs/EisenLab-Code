d0a18d21b0f06e132768cd2c7fac8f8737d21ebd
Peter Combs <peter.combs@berkeley.edu>
2012-04-20 16:03:36 -0700

    Add "MatchToSusan.py"
    A (still hacky) script to figure out which of Susan Lott's RNA-seq time
    points my RNA-seq data looks most like.  Does this by calculating a
    log-likelihood score, whereby the expression is assumed to be normally
    distributed around the mean of the all embryos at that time point, with
    std as the std of all those expression values.
    
    Yes, I'm aware that this is anywhere from 2-4 embryos per time point, so
    not great.  Yes, I'm aware that my embryo might not be from any of those
    time points. Yes, I'm aware that making the assumption that it's
    normally distributed is likely to be pretty crappy anyways. But it's a
    start.

---
+++ MatchToSusan.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from os import path
+from collections import defaultdict
+from scipy.stats import norm
+from numpy import isinf, log, finfo, float64, mean, std
+
+
+min_val = log(finfo(float64).tiny)
+susan_exprs = {}
+
+for fname in os.listdir('susan/by_cycle/'):
+    fbase = fname[:-4]
+    susan_exprs[fbase] = defaultdict(lambda : (0,100000))
+    for line in open(path.join('susan/by_cycle/', fname)):
+        if 'NAME' in line: continue
+        data = line.split()
+        gene_name = data[0]
+        mean_expr = mean([float(i) for i in data[1:]])
+        std_expr = std([float(i) for i in data[1:]]) or 1000
+        susan_exprs[fbase][gene_name] = (mean_expr, std_expr)
+        
+
+likelihoods = {time:0 for time in susan_exprs}
+
+for line in open(sys.argv[1]):
+    if "_name" in line: continue
+    data = line.split()
+    gene_name = data[0]
+    mean_expr = mean([float(i) for i in data[1:]])
+    for time in likelihoods:
+        L = norm.logpdf(mean_expr, *susan_exprs[time][gene_name])
+        if isinf(L):
+            # Not ideal, but since we're subtracting the minimum value, having
+            # something in range will always contribute.
+            print gene_name, time, mean_expr, susan_exprs[time][gene_name]
+            assert False
+            continue
+        else:
+            likelihoods[time] += L
+
+min_val = min(val for val in likelihoods.itervalues())
+max_val = max(val for val in likelihoods.itervalues())
+
+for time in likelihoods:
+    likelihoods[time] -= min_val
+    likelihoods[time] /= (max_val - min_val)
+
+print likelihoods
