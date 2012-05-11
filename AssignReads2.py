7f78199c9a03b2f6047b94c10c8d1971afbeebd6
Peter Combs <peter.combs@berkeley.edu>
2012-05-11 16:05:48 -0700

    Create AssignReads2
    This works by using the NM and NH tags in the samfile to make assignment calls.
    The first version of this that I wrote goes by looking at the 0x100 flag, which
    according to the documentation, corresponds to a read being the secondary
    alignment. I'm blown away that ever worked, and if it did, it seems like it'd
    be super conservative.   This works by keeping track of the best-mapping
    location for each read from each species (as defined by edit distance), then
    when all the mappings for that read are in, it figures out which ones can be
    unambiguously assigned.  It also attempts to figure out how large the
    delta-edit-distance between the first and second best species is.

---
+++ AssignReads2.py
@@ -0,0 +1,83 @@
+import pysam
+import sys
+from os import path
+from collections import defaultdict, Counter
+from progressbar import ProgressBar, ETA, Bar, Percentage
+from numpy import histogram
+
+ambig_threshold = 3
+
+for fname in sys.argv[1:]:
+    print fname
+    samfile = pysam.Samfile(fname, 'rb')
+    dir = path.dirname(fname)
+    assigned = pysam.Samfile(path.join(dir, 'assigned.bam'), 'wb',
+                             template=samfile)
+    ambig = pysam.Samfile(path.join(dir, 'ambiguous.bam'), 'wb',
+                             template=samfile)
+
+    to_be_resolved_reads = defaultdict(dict)
+    to_be_resolved_vals = defaultdict(lambda : defaultdict(lambda : 1000))
+    to_be_resolved_counts = Counter()
+    species_counts = Counter()
+    ambig_counts = Counter()
+
+    start = samfile.tell()
+    for read in samfile: pass
+    maxval = samfile.tell()
+    pbar = ProgressBar(maxval=maxval, widgets = [fname, ': ', Percentage(), ' ',
+                                                Bar(), ' ', ETA(), ' '])
+    samfile.seek(start)
+    pbar.start()
+
+    for read in samfile:
+        pbar.update(samfile.tell())
+        if not read.tags:
+            print read
+            print read.tags
+            assert False
+        nh = [t[1] for t in read.tags if t[0] == 'NH'][0]
+        species = samfile.references[read.rname].split('_')[0]
+        if nh == 1:
+            assigned.write(read)
+            species_counts[species] += 1
+            continue
+        else:
+            nm = [t[1] for t in read.tags if t[0] == 'NM'][0]
+            to_be_resolved_counts[read.qname] += 1
+            if nm < to_be_resolved_vals[read.qname][species]:
+                to_be_resolved_vals[read.qname][species] = nm
+                to_be_resolved_reads[read.qname][species] = read
+
+            if nh == to_be_resolved_counts[read.qname]:
+                # Sort out the reads
+                if len(to_be_resolved_reads[read.qname]) == 1:
+                    # Looks like there were multiple hits from the same species.
+                    # Report the best, or if equal quality, the first (which
+                    # tophat would've given anyways)
+                    assigned.write(read)
+                else:
+                    # Hits from multiple species
+                    vals = sorted([(val, spec) for spec, val in
+                                    to_be_resolved_vals[read.qname].iteritems()])
+                    diff_val = vals[1][0] - vals[0][0]
+                    ambig_counts[diff_val] += 1
+                    if diff_val > ambig_threshold:
+                        assigned.write(to_be_resolved_reads[read.qname][vals[0][1]])
+                        species_counts[vals[0][1]] += 1
+                    else:
+                        species_counts['ambig'] += 1
+                        for amb_read in to_be_resolved_reads[read.qname].itervalues():
+                            ambig.write(amb_read)
+
+                # Clean up the dictionaries
+                to_be_resolved_vals.pop(read.qname)
+                to_be_resolved_counts.pop(read.qname)
+                to_be_resolved_reads.pop(read.qname)
+
+    print
+    print "Species assignments: ", species_counts
+    print "Ambiguity distribution: ", ambig_counts
+
+            
+
