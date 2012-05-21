943d815773d1741c2c21daab5ce7c64a15c7e087
Peter Combs <peter.combs@berkeley.edu>
2012-05-21 13:33:04 -0700

    Refactor AssignSpecies2 into functions for easy profiling

--- AssignReads2.py
+++ AssignReads2.py
@@ -5,6 +5,64 @@ from collections import defaultdict, Counter
 from progressbar import ProgressBar, ETA, Bar, Percentage
 from numpy import histogram
 
+def get_nh(read):
+    return [t[1] for t in read.tags if t[0] == 'NH'][0]
+
+def get_species(read):
+    return references[read.rname].split('_')[0]
+
+def process_read(read):
+    if not read.tags:
+        print read
+        print read.tags
+        assert False
+    nh = get_nh(read)
+    species = get_species(read)
+    if nh == 1:
+        assigned.write(read)
+        species_counts[species] += 1
+        return
+    else:
+        resolve_multiread(read, nh, species)
+
+def resolve_multiread(read, nh, species):
+    nm = [t[1] for t in read.tags if t[0] == 'NM'][0]
+    to_be_resolved_counts[read.qname] += 1
+    if nm < to_be_resolved_vals[read.qname][species]:
+        to_be_resolved_vals[read.qname][species] = nm
+        to_be_resolved_reads[read.qname][species] = read
+
+    if nh == to_be_resolved_counts[read.qname]:
+        on_last_multiread(read)
+
+def on_last_multiread(read):
+    # Sort out the reads
+    if len(to_be_resolved_reads[read.qname]) == 1:
+        # Looks like there were multiple hits from the same species.
+        # Report the best, or if equal quality, the first (which
+        # tophat would've given anyways)
+        assigned.write(read)
+        species = references[read.rname].split('_')[0]
+        species_counts[species] += 1
+    else:
+        # Hits from multiple species
+        vals = sorted([(val, spec) for spec, val in
+                        to_be_resolved_vals[read.qname].iteritems()])
+        diff_val = vals[1][0] - vals[0][0]
+        ambig_counts[diff_val] += 1
+        if diff_val > ambig_threshold:
+            assigned.write(to_be_resolved_reads[read.qname][vals[0][1]])
+            species_counts[vals[0][1]] += 1
+        else:
+            species_counts['ambig'] += 1
+            for amb_read in to_be_resolved_reads[read.qname].itervalues():
+                ambig.write(amb_read)
+
+    # Clean up the dictionaries
+    to_be_resolved_vals.pop(read.qname)
+    to_be_resolved_counts.pop(read.qname)
+    to_be_resolved_reads.pop(read.qname)
+
 ambig_threshold = 3
 
 for fname in sys.argv[1:]:
@@ -31,50 +89,8 @@ for fname in sys.argv[1:]:
     pbar.start()
 
     for read in samfile:
-        pbar.update(samfile.tell())
-        if not read.tags:
-            print read
-            print read.tags
-            assert False
-        nh = [t[1] for t in read.tags if t[0] == 'NH'][0]
-        species = samfile.references[read.rname].split('_')[0]
-        if nh == 1:
-            assigned.write(read)
-            species_counts[species] += 1
-            continue
-        else:
-            nm = [t[1] for t in read.tags if t[0] == 'NM'][0]
-            to_be_resolved_counts[read.qname] += 1
-            if nm < to_be_resolved_vals[read.qname][species]:
-                to_be_resolved_vals[read.qname][species] = nm
-                to_be_resolved_reads[read.qname][species] = read
-
-            if nh == to_be_resolved_counts[read.qname]:
-                # Sort out the reads
-                if len(to_be_resolved_reads[read.qname]) == 1:
-                    # Looks like there were multiple hits from the same species.
-                    # Report the best, or if equal quality, the first (which
-                    # tophat would've given anyways)
-                    assigned.write(read)
-                else:
-                    # Hits from multiple species
-                    vals = sorted([(val, spec) for spec, val in
-                                    to_be_resolved_vals[read.qname].iteritems()])
-                    diff_val = vals[1][0] - vals[0][0]
-                    ambig_counts[diff_val] += 1
-                    if diff_val > ambig_threshold:
-                        assigned.write(to_be_resolved_reads[read.qname][vals[0][1]])
-                        species_counts[vals[0][1]] += 1
-                    else:
-                        species_counts['ambig'] += 1
-                        for amb_read in to_be_resolved_reads[read.qname].itervalues():
-                            ambig.write(amb_read)
-
-                # Clean up the dictionaries
-                to_be_resolved_vals.pop(read.qname)
-                to_be_resolved_counts.pop(read.qname)
-                to_be_resolved_reads.pop(read.qname)
-
+        pbar.update(samfile.tell() - start)
+        process_read(read)
     print
     print "Species assignments: ", species_counts
     print "Ambiguity distribution: ", ambig_counts
