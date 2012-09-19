28efe657c2f862b9ba3ea45f3a714306099566a1
Peter Combs <peter.combs@berkeley.edu>
2012-09-18 18:51:09 -0700

    Make Multigenome now makes genomes for appropriate pairs of genomes
    With the new run of data, I think I'll try just mapping to the species that I
    know are there.  This should make the downstream stuff even easier/faster.
    Hooray!

--- MakeMultigenome.py
+++ MakeMultigenome.py
@@ -3,14 +3,23 @@ from os import listdir, chdir
 from sys import argv
 
 chdir(argv[1])
-out_fh = open('multi.fa', 'w')
+base = argv[2]
+rest = argv[3:]
 
-for fname in listdir('.'):
-    if not fname.startswith('d'): continue
-    specname = fname.split('-')[0]
-    print specname
+print "Base species: ", base
+print "Other species: ", rest
 
-    for rec in SeqIO.parse(fname, 'fasta'):
-        rec.id = specname + '_' + rec.id
-        SeqIO.write(rec, out_fh, 'fasta')
+for other in rest:
+    print base+other
+    out_fh = open(base + other + '.fa', 'w')
+
+    for fname in listdir('.'):
+        if fname.startswith('d') and '.fa' in fname and ((base in fname) or (other in fname)):
+            specname = fname.split('-')[0]
+            print specname
+
+            for rec in SeqIO.parse(fname, 'fasta'):
+                rec.id = specname + '_' + rec.id
+                SeqIO.write(rec, out_fh, 'fasta')
+    out_fh.close()
 
