a5651382a2be7b9e99e21ed8460f8eb37cea8d35
Peter Combs <peter.combs@berkeley.edu>
2012-11-08 14:25:58 -0800

    Various scripts to fix the assigned reads
    RescueAmbiguous looks for read pairs where one end is unambiguously
    assigned, and where the other is not, and uses the unambiguous one to
    "rescue" the ambiguous one.
    
    FixAssignedBam goes through the reads and fixes the various SAM columns
    based on which ones actually got unambiguously assigned, rather than
    what the mapper said.
    
    CalculateConcentrations reads a tsv file with the read numbers and
    carrier concentrations, and estimates the original D. mel concentration.
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch Fall2012
    # Your branch is ahead of 'origin/Fall2012' by 2 commits.
    #
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	new file:   CalculateConcentrations.py
    #	new file:   FixAssignedBam.py
    #	new file:   RescueAmbiguous.py
    #	modified:   Utils.py
    #

---
+++ RescueAmbiguous.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+import pysam
+from collections import defaultdict
+from Utils import get_bam_length
+from progressbar import ProgressBar, Percentage, Bar, ETA
+import sys
+from os import path
+
+# SAM TAG Table
+is_multiple = 0x1           #template having multiple fragments in sequencing
+is_aligned = 0x2            #each fragment properly aligned according to the aligner
+is_unmapped = 0x4           #fragment unmapped
+is_next_unmapped = 0x8      #next fragment in the template unmapped
+is_revcomp = 0x10           #SEQ being reverse complemented
+is_next_reversed = 0x20     #SEQ of the next fragment in the template being reversed 
+is_read1 = 0x40             #the first fragment in the template
+is_read2 = 0x80             #the last fragment in the template
+is_secondary = 0x100        #secondary alignment
+is_failqc = 0x200           #not passing quality controls 
+is_dupe = 0x400             #PCR or optical duplicate
+
+
+for fname in sys.argv[1:]:
+    data = defaultdict(lambda : [None, None])
+    infile = pysam.Samfile(fname)
+    outfile = pysam.Samfile(fname[:-4] + '_rescued_unsorted.bam', 'wb',
+                            template=infile)
+    maxval, start = get_bam_length(infile)
+
+
+    pbar = ProgressBar(maxval=maxval - start,
+                       widgets = [fname, ': ', Percentage(), ' ', Bar(), ' ',
+                                  ETA(), ' '])
+    pbar.start()
+
+    for read in infile:
+        pbar.update(infile.tell() - start)
+        qname = read.qname
+        data[qname][read.is_read2] = read
+
+        if None not in data[qname]:
+            read1, read2 = data.pop(qname)
+            is_same = read1.rname == read2.rname
+            read1.rnext = read2.rname
+            read2.rnext = read1.rname
+            read1.pnext = read2.pos
+            read2.pnext = read1.pos
+            dist = abs(read2.pos - read1.pos) if is_same else 0
+            read1.tlen = dist
+            read2.tlen = -dist
+            read1.flag = (is_multiple + is_aligned + 
+                          read1.is_reverse * is_revcomp +
+                          read2.is_reverse * is_next_reversed + 
+                          is_read1)
+            read2.flag = (is_multiple + is_aligned + 
+                          read2.is_reverse * is_revcomp +
+                          read1.is_reverse * is_next_reversed + 
+                          is_read2)
+            assert read1.flag & 0x1
+            outfile.write(read1)
+            outfile.write(read2)
+
+    pbar.finish()
+
+    irefs = infile.references
+    ambig = pysam.Samfile(path.join(path.dirname(fname) , 'ambiguous.bam'))
+    arefs = ambig.references
+
+    imperfect = len(data)
+    print fname, ": ", imperfect, "\tImperfectly matched"
+
+    maxval, start = get_bam_length(ambig)
+    pbar = ProgressBar(maxval=maxval - start,
+                       widgets = [fname, ': ', Percentage(), ' ', Bar(), ' ',
+                                  ETA(), ' '])
+    pbar.start()
+    for read in ambig:
+        pbar.update(ambig.tell() - start)
+
+        qname = read.qname
+        if ((arefs[read.rname] in irefs) and 
+            (read.qname in data) and
+            (irefs[data[qname][read.is_read1].rname] == arefs[read.rname])):
+
+            data[qname][read.is_read2] = read
+            if None not in data[qname]:
+                read1, read2 = data.pop(qname)
+                is_same = read1.rname == read2.rname
+                read1.rnext = read2.rname
+                read2.rnext = read1.rname
+                read1.pnext = read2.pos
+                read2.pnext = read1.pos
+                dist = abs(read2.pos - read1.pos) if is_same else 0
+                read1.tlen = dist
+                read2.tlen = -dist
+                read1.flag = (is_multiple + is_aligned + 
+                              read1.is_reverse * is_revcomp +
+                              read2.is_reverse * is_next_reversed + 
+                              is_read1)
+                read2.flag = (is_multiple + is_aligned + 
+                              read2.is_reverse * is_revcomp +
+                              read1.is_reverse * is_next_reversed + 
+                              is_read2)
+                assert read1.flag & 0x1
+                outfile.write(read1)
+                outfile.write(read2)
+
+
+    pbar.finish()
+    rescued = imperfect - len(data)
+    print fname, ": ", rescued, "\trescued by ambiguous reads"
+    pbar = ProgressBar(widgets=['Finishing :', Percentage(), ' ', Bar(), ' ',
+                                ETA(), ' '])
+    for key in pbar(data):
+        read1, read2 = data[key]
+        if read1 and read1.flag & 0x4:
+            print read1
+        if read2 and read2.flag & 0x4:
+            print read
+
+        if read1 is None and read2 is None:
+            print "Somehow we ended up with a double empty"
+            assert False
+        elif read1 is not None and read2 is not None:
+            print "Somehow we didn't clear out the double-map"
+            assert False
+        elif read2 is None:
+            read1.rnext = -1
+            read1.pnext = 0
+            read1.tlen = 0
+            read1.flag = read1.flag | 0x8
+            outfile.write(read1)
+        elif read1 is None:
+            read2.rnext = -1
+            read2.pnext = 0
+            read2.tlen = 0
+            read2.flag = read2.flag | 0x8
+            outfile.write(read2)
+        else:
+            print "How did we get here?"
+            assert False
--- Utils.py
+++ Utils.py
@@ -1,4 +1,10 @@
+from os import path
 
+def get_bam_length(samfile):
+    start = samfile.tell()
+    maxval = path.getsize(samfile.filename) * 2**16 
+    # I don't know why that 2**16 factor is there!
+    return maxval +  2**16, start
 
 def strip_to_number(dataval, chars = '\'" \t #'):
     return to_number(dataval.strip(chars))
