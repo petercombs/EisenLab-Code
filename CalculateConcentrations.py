bf368fdc9099940006e332318e1b87f73c356a08
Peter Combs <peter.combs@berkeley.edu>
2012-11-26 15:45:26 -0800

    Fix CalculateConcentrations to work on the file as given

--- CalculateConcentrations.py
+++ CalculateConcentrations.py
@@ -2,14 +2,18 @@ from __future__ import division
 import pandas
 
 def main():
-    df = pandas.read_table('CountConfig.tab')
-    df.dropna()
+    df = pandas.read_table('CountConfig.tab', index_col = 5)
+    df.dropna(how='any')
 
-    for row in df:
-        mel_reads = row.mel_reads
-        carrier_reads = row.carrier_reads
-        carrier_conc = row.carrier_conc
+    for rowname, row in df.iterrows():
+        mel_reads = row['mel_reads']
+        carrier_reads = row['carrier_reads']
+        carrier_conc = row['carrier_conc']
 
-        mel_conc = mel_reads * carrier_reads / carrier_conc
-        print row.sample,
+        mel_conc = carrier_conc * mel_reads / carrier_reads 
+        print rowname,
         print mel_conc, 'ng total RNA'
+    return df
+
+if __name__ == "__main__":
+    df = main()
