dcd0ad713a90b8942ed7d04aef17dd07f42212f8
Peter Combs <peter.combs@berkeley.edu>
2013-01-09 18:09:30 -0800

    Make base genome data files and bowtie-builds
    Gotta make sure to do EVERYTHING we need.

--- MakeMultigenome.py
+++ MakeMultigenome.py
@@ -1,6 +1,7 @@
 from Bio import SeqIO
 from os import listdir, chdir
 from sys import argv
+from subprocess import Popen
 
 chdir(argv[1])
 base = argv[2]
@@ -9,6 +10,22 @@ rest = argv[3:]
 print "Base species: ", base
 print "Other species: ", rest
 
+## Make the BASE_only.fa file
+only_out_fh = open(base+"_only.fa", "w")
+for fname in listdir('.'):
+    if fname.startswith('d') and ('.fa' in fname) and (base in fname):
+        specname = fname.split('-')[0]
+        print specname
+
+        for rec in SeqIO.parse(fname, 'fasta'):
+            rec.id = specname + '_' + rec.id
+            SeqIO.write(rec, only_out_fh, 'fasta')
+
+only_out_fh.close()
+
+bowtie_build_procs = []
+
+## Make the joint fasta files with each species
 for other in rest:
     print base+other
     out_fh = open(base + other + '.fa', 'w')
@@ -22,8 +39,20 @@ for other in rest:
                 rec.id = specname + '_' + rec.id
                 SeqIO.write(rec, out_fh, 'fasta')
     out_fh.close()
+    bowtie_build_procs.append(Popen(['bowtie2-build', out_fh.name, base+other]))
 
+## Make the BASE_only gtf file
+only_out_fh = open(base + '_only.gtf', 'w')
+for fname in listdir('.'):
+    if fname.startswith('d') and ('.gtf' in fname) and (base in fname):
+        specname = fname.split('-')[0]
+        print specname
 
+        for line in open(fname):
+            only_out_fh.write(specname+"_"+line)
+only_out_fh.close()
+
+## Make the joint gtf files with each species
 for other in rest:
     print "GTF", base+other
     out_fh = open(base + other + '.gtf', 'w')
@@ -38,3 +67,5 @@ for other in rest:
     out_fh.close()
 
 
+for proc in bowtie_build_procs:
+    proc.wait()
