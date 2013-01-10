7636d38d75cd3245d20163c5972aab14d49b9e60
Peter Combs <peter.combs@berkeley.edu>
2013-01-09 18:10:54 -0800

    First attempt at a pared-down cufflinks/cuffdiff script
    At the moment, only doing the cuffdiff call.  That means we're not getting the
    individual values for each of the samples, but for all the analyses, I'm using
    the combined data only anyways.

---
+++ CalculateAbundances.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from collections import defaultdict
+from os import path
+
+design_file = pd.read_table('analysis-multi/design.tab')
+files = defaultdict(list)
+for ix, row in design_file.iterrows():
+    files[row['condition']].append(path.join('analysis-multi',
+                                          row['Sample'],
+                                          'dmel_reheadered_rescued_sorted.bam')
+                                )
+conditions = sorted(files.keys())
+
+cd_base = 'cuffdiff -p 4 -L {conditions} -o {outdir} -u -N -b {fasta} {gtf} {bams}'
+cd = cd_base.format(conditions = ','.join(conditions),
+                    outdir = 'analysis',
+                    fasta = 'Reference/AAA/mel_only.fa',
+                    gtf = 'Reference/AAA/mel_only.gtf',
+                    bams = ' '.join([','.join(files[key]) for key in conditions]))
+
+print cd
