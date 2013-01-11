3f34d2755baa7acbbecafcd8e0d3e7f852ee1f2b
Peter Combs <peter.combs@berkeley.edu>
2013-01-10 17:06:53 -0800

    Run all cufflinks-suite tools
    Also, clean up files so that they're pretty accesible towards the top of the
    file, and relatively easy to find and modify.

--- CalculateAbundances.py
+++ CalculateAbundances.py
@@ -1,6 +1,17 @@
 import pandas as pd
 from collections import defaultdict
 from os import path
+from subprocess import Popen
+
+fasta_ref = 'Reference/AAA/mel_only.fa',
+gtf_ref = 'Reference/AAA/mel_only.gtf'
+
+Popen(['cuffcompare',
+       '-o', path.join(path.dirname(gtf_ref), 'cuffcmp.'),
+       '-s', fasta_ref,
+       '-CG', '-r', gtf_ref, gtf_ref]).wait()
+
+gtf_ref = path.join(path.dirname(gtf_ref), 'cuffcmp.') + 'combined.gtf'
 
 design_file = pd.read_table('analysis-multi/design.tab')
 files = defaultdict(list)
@@ -11,11 +22,20 @@ for ix, row in design_file.iterrows():
                                 )
 conditions = sorted(files.keys())
 
-cd_base = 'cuffdiff -p 4 -L {conditions} -o {outdir} -u -N -b {fasta} {gtf} {bams}'
+cd_base = 'cuffdiff -p 8 -L {conditions} -o {outdir} -u -N -b {fasta} {gtf} {bams}'
 cd = cd_base.format(conditions = ','.join(conditions),
                     outdir = 'analysis',
-                    fasta = 'Reference/AAA/mel_only.fa',
-                    gtf = 'Reference/AAA/mel_only.gtf',
+                    fasta = fasta_ref,
+                    gtf = gtf_ref,
                     bams = ' '.join([','.join(files[key]) for key in conditions]))
 
 print cd
+Popen(cd).wait()
+cl_base = 'cufflinks -p 8 -o {outdir} -u -N -b {fasta} -G {gtf} {bamfile}'
+for condition in files:
+    for file in files[condition]:
+        cl = cl_base.format(outdir = path.dirname(file),
+                           fasta = fasta_ref,
+                           gtf = gtf_ref,
+                           bamfile = file)
+        Popen(cl).wait()
