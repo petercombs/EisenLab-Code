b7a604a62124178c2b4ddbc7f39c119dfc79263a
Peter Combs <peter.combs@berkeley.edu>
2013-07-31 08:53:31 -0700

    Add thousands separators to CountAllReads

--- CountAllReads.py
+++ CountAllReads.py
@@ -9,7 +9,7 @@ for dirname in sorted([d for d in glob('sequence/*') if path.isdir(d)]):
         extra_gzip = Popen(['gzip', '-d', '-c', fnames[-1]], stdout=PIPE)
         extra_wc = Popen(['wc', '-l'], stdin=extra_gzip.stdout, stdout=PIPE)
         n += int(extra_wc.communicate()[0].strip()) / 4
-        print dirname, n
+        print dirname, '{:,}'.format(n)
     except:
         print dirname, "ERR!"
 
