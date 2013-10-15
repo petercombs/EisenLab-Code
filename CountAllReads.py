15738e4839edef3b0df1242cfa353682769e742c
Peter Combs <peter.combs@berkeley.edu>
2013-10-14 17:10:35 -0700

    Cosmetic tweaks to CountAllReads
    Including ability to pass in directory from the command line, and to
    mark the output if there are a non-multiple of 4 number of lines.

--- CountAllReads.py
+++ CountAllReads.py
@@ -1,15 +1,21 @@
 from os import path
 from glob import glob
 from subprocess import Popen, PIPE
+from sys import argv
 
-for dirname in sorted([d for d in glob('sequence/*') if path.isdir(d)]):
+if len(argv) == 1:
+    dir = 'sequence'
+else:
+    dir = argv[1]
+
+for dirname in sorted([d for d in glob(path.join(dir, '*')) if path.isdir(d)]):
     try:
         fnames = sorted(glob(path.join(dirname, '*_R1_*.fastq.gz')))
         n = 4e6 * (len(fnames) - 1)
         extra_gzip = Popen(['gzip', '-d', '-c', fnames[-1]], stdout=PIPE)
         extra_wc = Popen(['wc', '-l'], stdin=extra_gzip.stdout, stdout=PIPE)
         n += int(extra_wc.communicate()[0].strip()) / 4
-        print dirname, '{:,}'.format(n)
+        print dirname, '\t{:15,}'.format(int(n)), ('*' if n % 1 else '')*10
     except:
         print dirname, "ERR!"
 
