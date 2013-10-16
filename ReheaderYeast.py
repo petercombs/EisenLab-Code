b48e0b84db35c9def70ad67a8534c6164dd0041f
Peter Combs <peter.combs@berkeley.edu>
2013-10-15 19:21:32 -0700

    Fix how I deal with yeast
    SGD labels the fasta file one way, and the GFF file a different way, and
    so ReheaderYeast should (I hope) help reconcile that.
    
    Also, the changes to allow multiple samples get pooled in analysis broke
    the way I was handling the yeast in the config file. That *should* now
    be fixed.

---
+++ ReheaderYeast.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from sys import stdin
+
+chroms = {'scer_ref|NC_001133|': 'chrI',
+          'scer_ref|NC_001134|': 'chrII',
+          'scer_ref|NC_001135|': 'chrIII',
+          'scer_ref|NC_001136|': 'chrIV',
+          'scer_ref|NC_001137|': 'chrV',
+          'scer_ref|NC_001138|': 'chrVI',
+          'scer_ref|NC_001139|': 'chrVII',
+          'scer_ref|NC_001140|': 'chrVIII',
+          'scer_ref|NC_001141|': 'chrIX',
+          'scer_ref|NC_001142|': 'chrX',
+          'scer_ref|NC_001143|': 'chrXI',
+          'scer_ref|NC_001144|': 'chrXII',
+          'scer_ref|NC_001145|': 'chrXIII',
+          'scer_ref|NC_001146|': 'chrXIV',
+          'scer_ref|NC_001147|': 'chrXV',
+          'scer_ref|NC_001148|': 'chrXVI',
+          'scer_ref|NC_001224|': 'chrMito',
+         }
+
+for line in stdin:
+    if '@SQ' in line and 'scer' not in line:
+        # Drop out non-yeast sequence lines
+        continue
+    if '@SQ' not in line:
+        print line.strip()
+        continue
+    columns = line.strip().split('\t')
+    SN = columns[1][3:]
+    columns[1] = 'SN:'+chroms[SN]
+
+    print '\t'.join(columns)
+
