70bc7aa661a5a1ba80e7864d16e657ada07d15b2
Peter Combs <peter.combs@berkeley.edu>
2014-05-05 17:26:37 -0700

    Start looking at cutoff FPKM values

--- AnalyzeFoldChanges.py
+++ AnalyzeFoldChanges.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 from scipy.stats import linregress, scoreatpercentile
 from matplotlib.pyplot import figure, subplot, hist, title, \
-        savefig, tight_layout, close, xlim
+        savefig, tight_layout, close, xlim, legend
 
 """
 
@@ -25,13 +25,17 @@ expr.set_index('gene_short_name', inplace=True, verify_integrity=True)
 protocols = {c.split('_')[0] for c in expr.columns}
 all_samples = {}
 all_slopes = {}
+expr_min = .01
+expr_max = 1e3
+bin_step = 1
 print(protocols)
 
 for protocol in protocols:
     try:
         samples = expr.select(startswith(protocol+"_"), axis=1)
         samples = samples.select(contains('subset'), axis=1)
-        selector = lambda x: x.startswith('Dvir') and 3 < np.max(samples.ix[x]) < 1000
+        selector = lambda x: (x.startswith('Dvir')
+                              and expr_min < np.max(samples.ix[x]) < expr_max)
 
         x_values = np.array([float(c.split('V')[-1].split('_')[0])
                              for c in samples.columns])
@@ -40,15 +44,15 @@ for protocol in protocols:
         samplesN = samples.divide(normer, axis='index')
         samplesN /= (samplesN.ix[:,-1].mean() / x_values[-1])
 
-        samples = samplesN
+        #samples = samplesN
         all_samples[protocol] = samplesN
         #all_samples[protocol] = samples
 
-        slopes = pd.Series(index=samples.index)
-        intercepts = pd.Series(index=samples.index)
-        r_values = pd.Series(index=samples.index)
-        for i, gene in enumerate(samples.index):
-            res = linregress(x_values, samples.ix[gene])
+        slopes = pd.Series(index=samplesN.index)
+        intercepts = pd.Series(index=samplesN.index)
+        r_values = pd.Series(index=samplesN.index)
+        for i, gene in enumerate(samplesN.index):
+            res = linregress(x_values, samplesN.ix[gene])
             slopes.ix[gene] = res[0]
             intercepts.ix[gene] = res[1]
             r_values.ix[gene] = res[2]
@@ -58,17 +62,25 @@ for protocol in protocols:
         figure(figsize=(16,16))
         subplot(3,1,1)
         slopes = slopes.dropna()
-        hist(slopes.dropna(),bins=100, range=(-1,5))
+        slopes_by_expr = [slopes.ix[(10**(i) < samples.ix[:,-1]) *
+                                    (samples.ix[:,-1] < 10**(i+bin_step))]
+                          .dropna()
+                          for i in np.arange(0, np.log10(expr_max), bin_step)]
+        hist(slopes_by_expr,bins=np.linspace(0, 2, 100), range=(-0,2),
+             stacked=True, histtype='bar',normed=True,
+             label=['{} < FPKM < {}'.format(10**i, 10**(i+bin_step))
+                    for i in np.arange(0, np.log10(expr_max), bin_step)])
         title('Slopes')
-        xlim(-1,5)
+        legend()
+        xlim(-0,2)
         print("Slopes")
         print(np.median(slopes), "+/-",)
         print(scoreatpercentile(slopes, 75) - scoreatpercentile(slopes, 25))
 
         subplot(3,1,2)
         intercepts = intercepts.dropna()
-        hist(intercepts,bins=100, range=(-2,2))
-        xlim(-2,2)
+        hist(intercepts,bins=100, range=(-50,50))
+        xlim(-50,50)
         title('Intercepts')
         print("Intercepts")
         print(np.median(intercepts.dropna()), "+/-",)
