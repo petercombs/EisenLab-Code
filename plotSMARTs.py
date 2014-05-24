b1366769305a6ba5d22457e8bb371c8e58131b2c
Peter Combs <pcombs+eisenlabcluster@gmail.com>
2014-05-23 18:11:39 -0700

    Changes to plotSMARTs for publication ready figs

--- plotSMARTs.py
+++ plotSMARTs.py
@@ -1,23 +1,24 @@
 from matplotlib.pyplot import figure, plot, xlabel, ylabel, savefig, \
         xlim, ylim,gca, clf, legend, semilogx, hist
 import setcolor
-from numpy import arange
-from scipy.stats import scoreatpercentile
+from numpy import arange, std
+from scipy.stats import scoreatpercentile, bartlett
 
 print(outdir)
 
 x_truseq = [0, 5, 10, 20]
 x_values_few = [0, 50, 100, 200]
 x_values_many = [0, 10, 50, 100, 200]
+x_values_many = [0,  50, 100, 200]
 
 print("Done with setup")
 
 figure()
-p = plot(x_truseq, all_samples['Truseq'].T, 'w', alpha=0.01)
+p = plot(x_truseq, all_samples['Truseq'].T, 'k', alpha=0.01)
 xlabel('ng D. virilis')
-ylabel('normalized FPKM')
-setcolor.set_foregroundcolor(gca(), 'w')
-setcolor.set_backgroundcolor(gca(), 'k')
+ylabel('normalized expression')
+#setcolor.set_foregroundcolor(gca(), 'w')
+#setcolor.set_backgroundcolor(gca(), 'k')
 ylim(0, 40)
 savefig('{outdir}/TruSeqLines.png'.format(outdir=outdir), transparent=True, dpi=300)
 
@@ -27,10 +28,10 @@ print("Done with TruSeq Lines")
 figure()
 p = plot(x_values_few, all_samples['SMART2'].T, 'r', alpha=0.01)
 xlabel('pg D. virilis')
-ylabel('normalized FPKM')
+ylabel('normalized expression')
 ylim(0, 400)
-setcolor.set_foregroundcolor(gca(), 'w')
-setcolor.set_backgroundcolor(gca(), 'k')
+#setcolor.set_foregroundcolor(gca(), 'w')
+#setcolor.set_backgroundcolor(gca(), 'k')
 savefig('{outdir}/SMART2Lines.png'.format(outdir=outdir), transparent=True, dpi=300)
 print("Done with SMART2 Lines")
 
@@ -39,9 +40,9 @@ clf()
 p = plot(x_values_many, all_samples['SMART2dil2'].T, 'g', alpha=0.01)
 ylim(0, 400)
 xlabel('pg D. virilis')
-ylabel('normalized FPKM')
-setcolor.set_foregroundcolor(gca(), 'w')
-setcolor.set_backgroundcolor(gca(), 'k')
+ylabel('normalized expression')
+#setcolor.set_foregroundcolor(gca(), 'w')
+#setcolor.set_backgroundcolor(gca(), 'k')
 savefig('{outdir}/SMART2-dil2Lines.png'.format(outdir=outdir), transparent=True, dpi=300)
 print("Done with 2.5x dilution Lines")
 
@@ -50,53 +51,69 @@ clf()
 p = plot(x_values_many, all_samples['SMART2dil5'].T, 'c', alpha=0.01)
 ylim(0, 400)
 xlabel('pg D. virilis')
-ylabel('normalized FPKM')
-setcolor.set_backgroundcolor(gca(), 'k')
-setcolor.set_foregroundcolor(gca(), 'w')
+ylabel('normalized expression')
+#setcolor.set_backgroundcolor(gca(), 'k')
+#setcolor.set_foregroundcolor(gca(), 'w')
 savefig('{outdir}/SMART2-dil5Lines.png'.format(outdir=outdir), transparent=True, dpi=300)
 print("Done with 5x dilution Lines")
 
 clf()
 semilogx([1], [0]);
-hist([samples.ix[r_values > .9, -1], samples.ix[r_values<=.9, -1]], bins=10**(arange(-1, 4, .1)), normed=False, histtype='step', stacked=False, label=['r > .9', 'r < .9'])
+hist([samples.ix[r_values > .9, -1], samples.ix[r_values<=.9, -1]],
+     bins=10**(arange(np.floor(np.log10(expr_min)), 4, .1)),
+     normed=False, histtype='step', stacked=False, label=['r > .9', 'r < .9'])
 legend()
-xlabel('FPKM')
-setcolor.set_foregroundcolor(gca(), 'w')
-setcolor.set_backgroundcolor(gca(), 'k')
+xlabel('expression')
+#setcolor.set_foregroundcolor(gca(), 'w')
+#setcolor.set_backgroundcolor(gca(), 'k')
 savefig('{outdir}/{protocol}-r90.png'.format(protocol=protocol,outdir=outdir), transparent=True, dpi=300)
 print("Done with r=.90 histogram")
 
 clf()
-for c, p in zip('w r g c'.split(), 'Truseq SMART2 SMART2dil2 SMART2dil5'.split()):
+for c, p in zip('k r g c'.split(), 'Truseq SMART2 SMART2dil2 SMART2dil5'.split()):
 
-    fpkms = arange(0, 100, .1)
+    fpkms = arange(expr_min, 100, .1)
     pct5 = [scoreatpercentile(all_rs[p][all_samples_nonorm[p].ix[:,-1] > n], 5) for n in fpkms]
     semilogx(fpkms, pct5, c, label=p.replace('dil', '--'))
 legend(loc='lower right')
-xlabel('FPKM Cutoff')
+xlabel('Raw Expression Cutoff')
 ylabel('5th Percentile Correlation Coefficient')
-setcolor.set_foregroundcolor(gca(), 'w')
-setcolor.set_backgroundcolor(gca(), 'k')
-savefig('{outdir}/r5th-SMARTs.png'.format(outdir=outdir), transparent=True, dpi=300)
+#setcolor.set_foregroundcolor(gca(), 'w')
+#setcolor.set_backgroundcolor(gca(), 'k')
+savefig('{outdir}/r5th-SMARTs-{expmin}.png'.format(outdir=outdir, expmin=expr_min), transparent=True, dpi=300)
 print("Done with cutoff estimation")
 
 base = 'SMART2dil2'
 base_slopes = all_slopes[base]
 
 figure()
+betweens = []
+withins = []
 for other in 'SMART2 SMART2dil5'.split():
     other_slopes = all_slopes[other]
     in_both = set(base_slopes.index).intersection(other_slopes.index)
+    labelstr = ('Same pre-amplification'
+                if 'dil' in other else
+                'Different pre-amplification')
     plot(base_slopes.ix[in_both], other_slopes.ix[in_both], '.',
-         label='{} vs {}'.format(base, other))
+         label=labelstr)
+    print("SMART2dil2 vs {}".format(other))
+    withins.append(base_slopes.ix[in_both] + other_slopes.ix[in_both])
+    betweens.append(base_slopes.ix[in_both] - other_slopes.ix[in_both])
+    print("std within  ", std(withins[-1]))
+    print("std between ", std(betweens[-1]))
+    print(bartlett(withins[-1], betweens[-1]))
+xlabel('Slope 1')
+ylabel('Slope 2')
 
 legend(numpoints=1)
 
 xlim(.4, 1.5)
 ylim(.4, 1.5)
-setcolor.set_foregroundcolor(gca(), 'w')
-setcolor.set_backgroundcolor(gca(), 'k')
-savefig('{outdir}/noisesource.png'.format(outdir=outdir), transparent=True, dpi=300)
-print("Done with cutoff estimation")
+#setcolor.set_foregroundcolor(gca(), 'w')
+#setcolor.set_backgroundcolor(gca(), 'k')
+savefig('{outdir}/noisesource_{expmin}.png'.format(outdir=outdir,
+                                                   expmin=expr_min), transparent=True, dpi=300)
+print("Done with noise sourcing")
 
 
