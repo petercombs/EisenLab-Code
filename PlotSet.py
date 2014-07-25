8c3da03af73c7361e29c382dacfff009ec8f0473
Peter Combs <pcombs+eisenlab@gmail.com>
2014-07-24 17:50:41 -0700

    Add option for error bars to PlotSet

--- PlotSet.py
+++ PlotSet.py
@@ -4,9 +4,9 @@ quick and dirty scratch function. If it proves useful, should probably be rolled
 into PlotUtils.py
 
 """
-from numpy import linspace, mean
-from matplotlib.pyplot import (clf, subplot, plot, legend, xticks, yticks,
-                               xlabel, ylim, savefig, figure)
+from numpy import linspace, mean, std
+from matplotlib.pyplot import (clf, subplot, errorbar, plot, legend, xticks, yticks,
+                               xlabel, ylabel, ylim, savefig, figure)
 import pandas as pd
 
 try:
@@ -20,6 +20,7 @@ startswith = lambda y: lambda x: x.startswith(y)
 
 def PlotSet(theset, wt, zld,
             filter_zyg=False,
+            draw_errorbars=False,
             outfn=None, normstage='cyc14B'):
     """Plot a matched set of the genes provided, broken out by zld binding
 
@@ -53,30 +54,53 @@ def PlotSet(theset, wt, zld,
     for i, (w, z) in enumerate(zip([w11, w13, w14A, w14B],
                                    [z11, z13, z14A, z14B])):
         subplot(1, 4, i+1)
-        plot(linspace(0, 1, len(w.columns), endpoint=True),
-             mean(w.ix[myset.intersection(has_zld)]
-                  .divide(normer.ix[myset.intersection(has_zld)].mean(axis=1),
-                          axis=0)),
-             'b-*',
-             label='WT +ZLD')
-        plot(linspace(0, 1, len(w.columns), endpoint=True),
-             mean(w.ix[myset.difference(has_zld)]
-                  .divide(normer.ix[myset.intersection(has_zld)].mean(axis=1),
-                          axis=0)),
-             'b-o', markerfacecolor='w',
-             label='WT -ZLD')
-        plot(linspace(0, 1, len(z.columns), endpoint=True),
-             mean(z.ix[myset.intersection(has_zld)]
-                  .divide(normer.ix[myset.intersection(has_zld)].mean(axis=1),
-                          axis=0)),
-             'r-*',
-             label='zld- +ZLD')
-        plot(linspace(0, 1, len(z.columns), endpoint=True),
-             mean(z.ix[myset.difference(has_zld)]
-                  .divide(normer.ix[myset.intersection(has_zld)].mean(axis=1),
-                          axis=0)),
-             'r-o', markerfacecolor='w',
-             label='zld- -ZLD')
+        normw_y = (w.ix[myset.intersection(has_zld)]
+                   .divide(normer.ix[myset.intersection(has_zld)].mean(axis=1),
+                              axis=0))
+        normw_n = (w.ix[myset.difference(has_zld)]
+                   .divide(normer.ix[myset.intersection(has_zld)].mean(axis=1),
+                              axis=0))
+        normz_y = (z.ix[myset.intersection(has_zld)]
+                      .divide(normer.ix[myset.intersection(has_zld)].mean(axis=1),
+                              axis=0))
+        normz_n = (z.ix[myset.difference(has_zld)]
+                      .divide(normer.ix[myset.intersection(has_zld)].mean(axis=1),
+                              axis=0))
+        if draw_errorbars:
+            errorbar(linspace(0, 1, len(w.columns), endpoint=True),
+                     mean(normw_y), yerr=std(normw_y),
+                     fmt='b-*', label='WT +ZLD'
+                    )
+            errorbar(linspace(0, 1, len(w.columns), endpoint=True),
+                     mean(normw_n), yerr=std(normw_n),
+                     fmt='b-*', label='WT -ZLD'
+                    )
+            errorbar(linspace(0, 1, len(z.columns), endpoint=True),
+                     mean(normz_y), yerr=std(normz_y),
+                     fmt='r-*',
+                     label='zld- +ZLD')
+            errorbar(linspace(0, 1, len(z.columns), endpoint=True),
+                     mean(normz_n), yerr=std(normz_n),
+                     fmt='r-o', markerfacecolor='w',
+                     label='zld- -ZLD')
+
+        else:
+            plot(linspace(0, 1, len(w.columns), endpoint=True),
+                 mean(normw_y),
+                 'b-*',
+                 label='WT +ZLD')
+            plot(linspace(0, 1, len(w.columns), endpoint=True),
+                 mean(normw_n),
+                 'b-o', markerfacecolor='w',
+                 label='WT -ZLD')
+            plot(linspace(0, 1, len(z.columns), endpoint=True),
+                 mean(normz_y),
+                 'r-*',
+                 label='zld- +ZLD')
+            plot(linspace(0, 1, len(z.columns), endpoint=True),
+                 mean(normz_n),
+                 'r-o', markerfacecolor='w',
+                 label='zld- -ZLD')
         xticks([0, 1], 'AP')
         max_val = max(max_val, ylim()[1])
         if i != 0:
