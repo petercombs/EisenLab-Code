11b360be1cb7a65b951d195745b3e2bf7440a7fa
Peter Combs <pcombs+eisenlab@gmail.com>
2014-08-29 17:34:45 -0700

    Show Zelda mutants on the images as well.

--- MakeComparisonFiles.py
+++ MakeComparisonFiles.py
@@ -1,27 +1,31 @@
 import pandas as pd
 import PlotUtils
 from progressbar import ProgressBar
-from matplotlib.cm import Greens
+from matplotlib.cm import Greens, Reds
 import os
 from os.path import join
 from numpy import nan
 
 startswith = lambda x: lambda y: y.startswith(x)
 
-wt  = (pd.read_table('prereqs/WT6.01.summary.tsv', index_col=0,
-                                          keep_default_na=False,
-                     na_values=['---', ''])
+read_table_args = dict(index_col=0,
+                       keep_default_na=False,
+                       na_values=['---', ''])
+wt  = (pd.read_table('prereqs/WT6.01.summary.tsv', **read_table_args)
               .dropna(how='any')
               .sort_index())
-bcd = (pd.read_table('analysis/summary.tsv', index_col=0,
-                                          keep_default_na=False,
-                     na_values=['---', ''])
+bcd = (pd.read_table('analysis/summary.tsv', **read_table_args)
+              .dropna(how='any', axis=1)
+              .sort_index())
+zld = (pd.read_table('prereqs/Zld6.01.summary_merged.tsv', **read_table_args)
               .dropna(how='any', axis=1)
               .sort_index())
 assert all(wt.index == bcd.index)
+assert all(wt.index == zld.index)
+assert all(zld.index == bcd.index)
 
 try:
-        os.makedirs('analysis/results/svgs')
+        os.makedirs('analysis/results/svgs-withzld')
 except OSError:
         pass
 
@@ -31,25 +35,31 @@ for gene in pbar(wt.index):
         continue
     if gene is nan:
         continue
-    data = (wt .select(startswith('cyc13'), axis=1).ix[[gene]],
-            wt .select(startswith('cyc14D'), axis=1).ix[[gene]],
-            bcd.select(startswith('cyc13_rep1'), axis=1).ix[[gene]],
+    data = (wt .select(startswith('cyc13'),       axis=1).ix[[gene]],
+            wt .select(startswith('cyc14D'),      axis=1).ix[[gene]],
+            bcd.select(startswith('cyc13_rep1'),  axis=1).ix[[gene]],
             bcd.select(startswith('cyc14D_rep1'), axis=1).ix[[gene]],
-            bcd.select(startswith('cyc13_rep2'), axis=1).ix[[gene]],
+            bcd.select(startswith('cyc13_rep2'),  axis=1).ix[[gene]],
             bcd.select(startswith('cyc14D_rep2'), axis=1).ix[[gene]],
+            zld.select(startswith('cyc13_rep1'),  axis=1).ix[[gene]],
+            zld.select(startswith('cyc14D'),      axis=1).ix[[gene]],
+            zld.select(startswith('cyc13_rep3'),  axis=1).ix[[gene]],
            )
     PlotUtils.svg_heatmap(data,
                           filename=join('analysis/results/svgs',
                                         gene + '.svg'),
                           cmap=(None,None,
                                 Greens, Greens,
-                                Greens, Greens),
+                                Greens, Greens,
+                                Reds, Reds,
+                                Reds
+                               ),
                           norm_rows_by=(data[0].max(axis=1)*1.1+10,
-                                        data[1].max(axis=1)*1.1+10)*3,
+                                        data[1].max(axis=1)*1.1+10)*5[:-1],
                           draw_box=True,
                           draw_name=True,
                           data_names=('WT13 WT14D Bcd-13 Bcd-14D Bcd-13'
-                                     ' Bcd-14D').split(),
+                                     ' Bcd-14D Zld-13 Zld-14D Zld-13').split(),
                           total_width=400,
                           box_height=130,
                           max_width=840,
