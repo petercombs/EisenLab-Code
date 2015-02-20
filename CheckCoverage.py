c6b3d3ed49796b4792854e1bddd91db2e14f65e2
Peter Combs <pcombs+eisenlabcluster@gmail.com>
2015-02-20 13:48:35 -0800

    Multithread the Complexity Estimation

--- CheckCoverage.py
+++ CheckCoverage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import division
+from __future__ import division, print_function
 from collections import defaultdict
 import pysam
 import re
@@ -10,18 +10,22 @@ from scipy import stats
 from numpy import array, log10, exp
 import progressbar as pbar
 
+if sys.version_info.major < 3:
+    res = raw_input('Code very memory heavy in less than Python3. Type "Y" to continue')
+    assert res[0].upper() == 'Y'
+
 if len(sys.argv) < 3:
-    print """Usage: python CheckCoverage.py <GTF-File> BAMfile [BAMfile ...]"""
+    print("""Usage: python3 CheckCoverage.py <GTF-File> BAMfile [BAMfile ...]""")
     sys.exit(1)
 
 gtf_fname = sys.argv[1]  # 'Reference/dmel-all-r5.42.gtf'
 analysis_dir = 'analysis'
 
-starts = set()
-curr_len = -1
-coverage = 0
+#starts = set()
+#curr_len = -1
+#coverage = 0
 
-cutoff = 0
+#cutoff = 0
 fbtr_finder = re.compile('FBtr[0-9]*')
 
 
@@ -29,19 +33,25 @@ def analyze_bamfile(bam_fname):
     bam_file = pysam.Samfile(bam_fname, 'rb')
 
     coverages = defaultdict(lambda: [0, 0, set()])
-    parent = ''
-    curr_len = 0
-    coverage = 0
-    starts = set()
+    #parent = ''
+    #curr_len = 0
+    #coverage = 0
+    #starts = set()
 
     f = open(gtf_fname)
-    for line in f:
+    for i, line in enumerate(f):
         pass
     pb = pbar.ProgressBar(widgets=[bam_fname, pbar.Bar(), pbar.ETA()],
-                          maxval=f.tell()).start()
+                          maxval=i).start()
     f.seek(0)
-    for line in f:
-        pb.update(f.tell())
+
+    current_fbtr = None
+    last_fbtr = None
+    #from guppy import hpy
+    #hp = hpy()
+    #before = hp.heap()
+    for i, line in enumerate(f):
+        pb.update(i)
         if line.startswith('#'):
             continue
         if line.startswith('>'):
@@ -66,13 +76,25 @@ def analyze_bamfile(bam_fname):
                 coverage += 1
                 starts.add(read.pos)
             coverages[fbtr][0] += coverage
-            coverages[fbtr][2].update(starts)
+            try:
+                coverages[fbtr][2].update(starts)
+            except AttributeError:
+                print(last_fbtr, current_fbtr, fbtr)
+                continue
+            del(starts)
+            if current_fbtr and fbtr != current_fbtr:
+                coverages[current_fbtr][2] = len(coverages[current_fbtr][2])
+                last_fbtr = current_fbtr
+                current_fbtr = fbtr
+            elif current_fbtr is None:
+                current_fbtr = fbtr
 
     pb.finish()
-    rpks, curr_lens, uniques = zip(*coverages.itervalues())
+    coverages[fbtr][2] = len(coverages[fbtr][2])
+    rpks, curr_lens, uniques = zip(*coverages.values())
     dir, fname = path.split(bam_fname)
 
-    return fname, rpks, uniques, curr_lens
+    return bam_fname, rpks, uniques, curr_lens
 
 if __name__ == "__main__":
     import multiprocessing as mp
@@ -80,24 +102,27 @@ if __name__ == "__main__":
     POOL = mp.Pool(20)
     res = POOL.map(analyze_bamfile,
                    [f for f in sys.argv[2:] if f.endswith('.bam')])
+    #res = map(analyze_bamfile, [f for f in sys.argv[2:] if f.endswith('.bam')])
     all_dirs, all_rpks, all_pct_uniques, all_lens = zip(*res)
 
-    import cPickle as pickle
-    out_fh = open('checkcoverage.pkl', 'w')
-    pickle.dump({'dirs': all_dirs, 'rpks': all_rpks,
-                 'pct_uniques': all_pct_uniques, 'lens': all_lens},
-                out_fh)
+    import pickle
+    #out_fh = open('checkcoverage.pkl', 'w')
+    #pickle.dump({'dirs': all_dirs, 'rpks': all_rpks,
+                 #'pct_uniques': all_pct_uniques, 'lens': all_lens},
+                #out_fh)
     for fname, rpks, uniques, curr_lens in res:
-        print fname
+        print(fname)
         try:
             xs = array([rpk/(curr_len+1)
                        for rpk, curr_len in zip(rpks, curr_lens)])
-            ys = array([len(u)/(curr_len + 1)
+            ys = array([(u)/(curr_len + 1)
                        for u, curr_len in zip(uniques, curr_lens)])
-            cutoff = max(xs[ys < .1])
-            reg = stats.linregress(log10(xs[(xs < cutoff)*(xs > 0)*(ys > 0)]),
-                                   log10(ys[(xs < cutoff)*(xs > 0)*(ys > 0)]))
-            print "exp(%f) * x ** %f" % (reg[1], reg[0])
-            print "Duplicate badness score: ", 10**(-reg[1]-.031)
+            #cutoff = max(xs[ys < .1])
+            #reg = stats.linregress(log10(xs[(xs < cutoff)*(xs > 0)*(ys > 0)]),
+                                   #log10(ys[(xs < cutoff)*(xs > 0)*(ys > 0)]))
+            reg = stats.linregress(log10(xs[ys < .1]),
+                                   log10(ys[ys < .1]))
+            print("exp(%f) * x ** %f" % (reg[1], reg[0]))
+            print("Duplicate badness score: ", 10**(-reg[1]-.031))
         except Exception as exc:
-            print exc
+            print(exc)
