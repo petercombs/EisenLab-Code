07c17ce04fbf077bbd6f59a8a6e55340725e314f
Peter Combs <pcombs+eisenlab@gmail.com>
2015-03-27 16:33:05 -0700

    Excise old code from ThreeWay

--- ThreeWayHistograms.py
+++ ThreeWayHistograms.py
@@ -3,10 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as mpl
 from numpy import arange, array, histogram, abs, median, mean
 import DistributionDifference as DD
-from bisect import bisect
 from scipy.stats import scoreatpercentile, chi2_contingency, gaussian_kde
 import setcolor
-from collections import defaultdict
 from Utils import sel_startswith, sel_contains, contains
 from itertools import combinations
 from sys import argv
@@ -48,43 +46,6 @@ def earth_mover_multi(points1, points2, abs_expr=False):
 
 dist_func = earth_mover_multi
 
-try:
-    tss_dict = locals()['tss_dict']
-except KeyError:
-    tss_dict = defaultdict(list)
-    for i, row in pd.read_table('Reference/tss').iterrows():
-        tss_dict[row['chr'].replace('dmel_', '')].append((row['TSS_start'], row['gene_name']))
-
-    for chrom in tss_dict.itervalues():
-        chrom.sort()
-
-def find_near(chrom, coord, dist):
-    out = set()
-    center = bisect([i[0] for i in chrom], coord)
-    for (coord2, gene) in reversed(chrom[:center]):
-        if coord - coord2 > dist: break
-        out.add(gene)
-
-    for (coord2, gene) in chrom[center:]:
-        if coord2 - coord > dist: break
-        out.add(gene)
-
-    return out
-
-
-
-
-zld_bind = pd.read_table('Reference/zld_peaks')
-has_zld = set()
-for coord in zld_bind.NewPeak:
-    chr, coord = coord.split(':')
-    has_zld.update(find_near(tss_dict[chr], int(coord), bind_dist))
-
-bcd_bind = pd.read_table('Reference/bcd_peaks')
-has_bcd = set()
-for coord in bcd_bind.NewPeak:
-    chr, coord = coord.split(':')
-    has_bcd.update(find_near(tss_dict[chr], int(coord), bind_dist))
 
 def get_dists(gene):
     return (dist_func(set1.ix[gene], set2.ix[gene]),
@@ -326,138 +287,6 @@ if __name__ == "__main__":
                          sep='\t',
                         )
         all_d2s['_'.join([set1_name, set2_name, set3_name])] = dist2
-    mpl.clf()
-    fig1 = mpl.figure()
-    fig2 = mpl.figure()
-    for trio in all_d2s:
-        if not trio.startswith('wt'): continue
-        k = gaussian_kde(all_d2s[trio])
-        fig1.gca().hist(all_d2s[trio],
-                 bins=arange(0, .5, .0033),
-                 normed=True,
-                 label='{} 3-way distance'.format(trio),
-                 histtype='step'
-                )
-        fig2.gca().plot(arange(0, .5, 1e-4),
-                  k(arange(0, .5, 1e-4)),
-                  label='{} 3-way distance'.format(trio),
-                 )
-    max_y = max(fig1.gca().get_ylim()[1], fig2.gca().get_ylim()[1])
-    for f in (fig1, fig2):
-        f.gca().set_xlabel('D2 score (difference of distances)')
-        f.gca().set_ylabel('Density')
-        f.gca().set_ylim(0, max_y)
-        f.gca().legend()
-    fig1.savefig('analysis/results/all_d2s.png', dpi=300)
-    fig2.savefig('analysis/results/all_d2s_kde.png', dpi=300)
-    mpl.close('all')
-    assert False
-
-    keep = 'TRACK DOWN THIS LINE'
-
-    colors = pd.Series('y g b c'.split())
-    cmaps = pd.Series(index=wt.index, data=0, dtype=int)
-    for gene in cmaps.index:
-        for i, has_bind in enumerate([has_zld, has_bcd]):
-                cmaps.ix[gene] += 2**i * (gene in has_bind)
-
-    cmaps.sort(ascending=False)
-    cmaps2 = colors[cmaps]
-    cmaps2.index = cmaps.index
-
-    keep = keep.ix[cmaps.index]
-
-    wt_zld = bcd_zld = wt_bcd = 0
-
-    wt_zld_exp = wt_zld.ix[cmaps.index].ix[keep]
-    wt_bcd_exp = wt_bcd.ix[cmaps.index].ix[keep]
-    bcd_zld_exp = bcd_zld.ix[cmaps.index].ix[keep]
-    cmaps_exp =  cmaps .ix[cmaps.index].ix[keep]
-    dist2 = bcd_zld_exp - abs(wt_zld_exp - wt_bcd_exp)
-    dist2_sorted = dist2.copy()
-    dist2_sorted.sort()
-
-    ax = mpl.gca()
-
-    mpl.plot([0,1], [0,1], 'r:', zorder=5)
-    mpl.plot([0, xx, xx], [yy, yy, 0], 'k-', zorder=5, alpha=0.4)
-    for c in range(4):
-        mpl.scatter(x=wt_zld_exp.ix[(cmaps_exp == c)],
-                    y=wt_bcd_exp.ix[(cmaps_exp == c)],
-                    c='w',
-                    marker = '.',
-                    edgecolors='none',
-                    zorder=c+1,
-                    alpha=0.999999999)
-    mpl.xlabel('WT vs Zld')
-    mpl.ylabel('WT vs Bcd')
-    mpl.xlim(0, max(1, max(wt_zld_exp)*1.1))
-    mpl.ylim(0, max(1, max(wt_bcd_exp)*1.1))
-    mpl.title('{cyc} - {dist:3.1f}kb'
-              .format(cyc=cyc_of_interest,
-                      dist=bind_dist/1000.))
-    ax.set_aspect(1)
-    setcolor.set_foregroundcolor(mpl.gca(), 'w')
-    setcolor.set_backgroundcolor(mpl.gca(), 'b')
-    mpl.gcf().tight_layout()
-    mpl.savefig('analysis/results/WTBcdWTZldCorr-{}.png'.format(cyc_of_interest),
-                dpi=600, transparent=True)
-    ax.add_patch(mpatches.PathPatch(bcd_only, facecolor='b', alpha=1)).set_zorder(0)
-    ax.add_patch(mpatches.PathPatch(zld_only, facecolor='g', alpha=1)).set_zorder(0)
-    ax.add_patch(mpatches.PathPatch(both_change, facecolor='c', alpha=1)).set_zorder(0)
-    ax.add_patch(mpatches.PathPatch(no_change, facecolor='r', alpha=1)).set_zorder(0)
-    mpl.savefig('analysis/results/WTBcdWTZldCorr-{}-withpatches.png'.format(cyc_of_interest),
-                dpi=600, transparent=True)
 
     mpl.clf()
-    ax = mpl.gca()
-    mpl.scatter(x=wt_zld_exp.ix[dist2_sorted.index],
-                y=wt_bcd_exp.ix[dist2_sorted.index],
-                c=dist2.ix[dist2_sorted.index],
-                edgecolors=(0., 0., 0., 0.))
-    mpl.xlabel('WT vs Zld')
-    mpl.ylabel('WT vs Bcd')
-    ax.add_patch(mpatches.PathPatch(bcd_only, facecolor=(0., 0., 1., 0.1),
-                                    edgecolor=(.5, .5, .5, 1))).set_zorder(5)
-    ax.add_patch(mpatches.PathPatch(zld_only, facecolor=(0., 1., 0., 0.1),edgecolor=(.5, .5, .5, 1) )).set_zorder(5)
-    ax.add_patch(mpatches.PathPatch(both_change, facecolor=(0., 1., 1., 0.1), edgecolor=(.5, .5, .5, 1))).set_zorder(5)
-    ax.add_patch(mpatches.PathPatch(no_change, facecolor=(1., 0., 0., 0.1), edgecolor=(.5, .5, .5, 1))).set_zorder(5)
-    mpl.xlim(0, max(1, max(wt_zld_exp)*1.1))
-    mpl.ylim(0, max(1, max(wt_bcd_exp)*1.1))
-    ax.set_aspect(1)
-    mpl.colorbar()
-    setcolor.set_foregroundcolor(mpl.gca(), 'w')
-    setcolor.set_backgroundcolor(mpl.gca(), 'b')
-    mpl.savefig('analysis/results/WT_Zld_Bcd_3way.png', dpi=600, transparent=True)
-
-
-    in_bcd = bcd_only.contains_points(zip(wt_zld_exp, wt_bcd_exp))
-    in_zld = zld_only.contains_points(zip(wt_zld_exp, wt_bcd_exp))
-    in_both = both_change.contains_points(zip(wt_zld_exp, wt_bcd_exp))
-    in_nochg =  no_change.contains_points(zip(wt_zld_exp, wt_bcd_exp))
-
-    in_category = pd.Series(index=wt_zld_exp.index, data='')
-    for cat, genes in zip(['Bcd Only', 'Zld Only', 'Both', 'No Change'],
-                          [in_bcd, in_zld, in_both, in_nochg]):
-        in_category.ix[genes] = cat
-
-    in_category.to_csv('analysis/results/change_cats.txt', sep='\t')
-
-    contingency = array(
-        [histogram(cmaps_exp.ix[in_bcd],  bins=arange(5))[0],
-         histogram(cmaps_exp.ix[in_zld],  bins=arange(5))[0],
-         histogram(cmaps_exp.ix[in_both], bins=arange(5))[0]])
-
-    print(contingency)
-    print(chi2_contingency(contingency))
-
-    all_dists = pd.DataFrame(data={'WT_Zld': wt_zld_exp, 'WT_Bcd':wt_bcd_exp,
-                                   'Bcd_Zld': bcd_zld_exp, 'Dist2' : dist2},
-                            columns=['WT_Zld', 'WT_Bcd', 'Bcd_Zld', 'Dist2'])
-
-    all_dists.ix[in_bcd].sort(columns='Dist2').to_csv('analysis/results/change_bcd.tsv', sep='\t')
-
-    all_dists.ix[in_zld].sort(columns='Dist2').to_csv('analysis/results/change_zld.tsv', sep='\t')
-
-    all_dists.ix[in_both].sort(columns='Dist2').to_csv('analysis/results/change_both.tsv', sep='\t')
 
